0d1403f1437d68cc4bd7c48705962356e64facc6
leejahy25 <yyyuhaig2511@gmail.com>
2025-03-06 08:16:46 +0700

    add DetailMessageActivity.java

---
+++ app/src/main/java/com/example/selfalarm/DetailMessageActivity.java
@@ -0,0 +1,60 @@
+package com.example.selfalarm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DetailMessageActivity extends AppCompatActivity {
+//    private TextView tvTitle;
+//    private RecyclerView recyclerView;
+//    private ChatAdapter chatAdapter;
+//    private List<ChatMessage> chatMessages;
+//    private TextView tvSenderName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_detail_message);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.detail_message_screen), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        findViewById(R.id.btnBack).setOnClickListener(v -> finish());
+
+//        String sender = getIntent().getStringExtra("sender");
+//        if (sender != null) {
+//            tvTitle.setText(sender);
+//        }
+
+//        String senderName = getIntent().getStringExtra("sender");
+//        tvSenderName.setText(senderName);
+//
+//        // Hard code lịch sử chat
+//        loadChatHistory();
+//
+//        chatAdapter = new ChatAdapter(chatMessages);
+//        recyclerView.setAdapter(chatAdapter);
+    }
+
+//    private void loadChatHistory() {
+//        chatMessages = new ArrayList<>();
+//        chatMessages.add(new ChatMessage("Hey, how are you?", false));
+//        chatMessages.add(new ChatMessage("I'm good, what about you?", true));
+//        chatMessages.add(new ChatMessage("I'm fine too. Any plans for today?", false));
+//    }
+}

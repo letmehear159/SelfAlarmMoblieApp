eae2ff9d52364cc46bbfbf45bf48ee4f010be324
leejahy25 <yyyuhaig2511@gmail.com>
2025-03-06 08:15:44 +0700

    add ChatAdapter.java

---
+++ app/src/main/java/com/example/selfalarm/ChatAdapter.java
@@ -0,0 +1,55 @@
+package com.example.selfalarm;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+    private List<ChatMessage> chatMessages;
+
+    public ChatAdapter(List<ChatMessage> chatMessages) {
+        this.chatMessages = chatMessages;
+    }
+
+    @NonNull
+    @Override
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        if (viewType == 1) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat_sent, parent, false);
+        } else {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat_received, parent, false);
+        }
+        return new ChatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
+        holder.messageTextView.setText(chatMessages.get(position).getMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return chatMessages.get(position).isSentByUser() ? 1 : 0;
+    }
+
+    static class ChatViewHolder extends RecyclerView.ViewHolder {
+        TextView messageTextView;
+
+        public ChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            messageTextView = itemView.findViewById(R.id.tvChatMessage);
+        }
+    }
+}

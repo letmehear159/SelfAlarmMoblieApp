196ca99b550146de4d7a5ca5831bb2d05fa309d7
Nguyễn Trường <nguyentruongpro19@gmail.com>
2025-04-10 21:51:19 +0700

    update version sql

--- app/src/main/java/com/example/selfalarm/helper/DatabaseHelper.java
+++ app/src/main/java/com/example/selfalarm/helper/DatabaseHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "MixMobileAppDB";
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String TABLE_NAME = "Alarm";
     public static final String COLUMN_ID = "id";
     public static final String COLUMN_TIMESTAMP = "timestamp";

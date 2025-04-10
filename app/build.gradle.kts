0238665960f99043e8681e766d5225de90e567e0
leejahy25 <yyyuhaig2511@gmail.com>
2025-04-10 11:19:55 +0700

    add dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
